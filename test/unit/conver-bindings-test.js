9834293c6ad78c54d08eb4121f18e206f9b5b49d
Chase McCarthy <chase@code0100fun.com>
2017-11-08 12:16:26 -0600

    Add convert-bindings formula

---
+++ test/unit/conver-bindings-test.js
@@ -0,0 +1,20 @@
+import convertBindings from '../../lib/formulas/convert-bindings';
+import _printEqual from '../helpers/print-equal';
+
+const printEqual = (input, output) => {
+  _printEqual(input, output, { formulas: [convertBindings] });
+};
+
+describe('Unit: convertBindings', () => {
+  it('converts fooBinding="bar" to foo=bar', () => {
+    const input = '{{a fooBinding="bar.baz"}}';
+    const output = '{{a foo=bar.baz}}';
+    printEqual(input, output);
+  });
+
+  it('does not convert classBinding', () => {
+    const input = '{{a classBinding=":bar"}}';
+    const output = '{{a classBinding=":bar"}}';
+    printEqual(input, output);
+  });
+});
